6dfd4744bf41657c3178dc784177fd1cdcb55676
Linh Bouniol <paulalinh89@gmail.com>
2018-08-01 14:40:28 -0700

    Created local notificaitons

---
+++ Memories/Memories/LocalNotificationHelper.swift
@@ -0,0 +1,45 @@
+//
+//  LocalNotificationHelper.swift
+//  Memories
+//
+//  Created by Linh Bouniol on 8/1/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class LocalNotificationHelper {
+    
+    func getAuthorizationStatus(completion: @escaping (UNAuthorizationStatus) -> Void) {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            DispatchQueue.main.async {
+                completion(settings.authorizationStatus)
+            }
+        }
+    }
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            
+            if let error = error { NSLog("Error requesting authorization status for local notifications: \(error)") }
+            
+            DispatchQueue.main.async {
+                completion(success)
+            }
+        }
+    }
+    
+    func scheduleDailyReminderNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Did you create a memory today?"
+        content.body = "Don't forget to add photos to your memory."
+        content.sound = UNNotificationSound.default()
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 20
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+    }
+}
