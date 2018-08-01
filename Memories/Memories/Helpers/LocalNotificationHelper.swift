ad99157ae3b37e6d72c1dbc33edfd222ca5bb1e8
Conner Alegre <cga0@pm.me>
2018-08-01 16:36:43 -0600

    Updated Memory Edit/Add page with more constraints

---
+++ Memories/Memories/Helpers/LocalNotificationHelper.swift
@@ -0,0 +1,9 @@
+//
+//  LocalNotificationHelper.swift
+//  Memories
+//
+//  Created by Conner on 8/1/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import Foundation
---
+++ Memories/Memories/View Controllers/MemoryDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MemoryDetailViewController.swift
+//  Memories
+//
+//  Created by Conner on 8/1/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class MemoryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addPhoto(_ sender: Any) {
+    }
+    
+    @IBAction func saveMemory(_ sender: Any) {
+    }
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var descriptionTextView: UITextView!
+}
