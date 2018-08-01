5c6522282d0e0a6e6ca7d6e8c958e55c0852b0ed
Linh Bouniol <paulalinh89@gmail.com>
2018-08-01 14:14:02 -0700

    Created storyboards

---
+++ Memories/Memories/View Controllers/MemoryDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MemoryDetailViewController.swift
+//  Memories
+//
+//  Created by Linh Bouniol on 8/1/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import UIKit
+
+class MemoryDetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var textView: UITextView!
+    
+    @IBAction func addPhoto(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
