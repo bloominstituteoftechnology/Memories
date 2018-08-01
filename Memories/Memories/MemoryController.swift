9f27b2b71730a12fe0778bf6cb85ae20667a686b
Conner Alegre <cga0@pm.me>
2018-08-01 13:56:39 -0600

    Add ability to create, update and delete memories

--- Memories/Memories/MemoryController.swift
+++ Memories/Memories/MemoryController.swift
@@ -9,6 +9,30 @@
 import Foundation
 
 class MemoryController {
+    func createMemory(title: String, bodyText: String, imageData: Data) {
+        let memory = Memory(title: title, bodyText: bodyText, imageData: imageData)
+        memories.append(memory)
+    }
+
+    func updateMemory(memory: Memory) {
+        if let index = memories.index(of: memory) {
+            var scratch = memories[index]
+            scratch.bodyText = memory.bodyText
+            scratch.imageData = memory.imageData
+            scratch.title = memory.title
+
+            memories.remove(at: index)
+            memories.insert(scratch, at: index)
+
+            saveToPersistentStore()
+        }
+    }
+
+    func deleteMemory(memory: Memory) {
+        guard let index = memories.index(of: memory) else { return }
+        memories.remove(at: index)
+    }
+
     func saveToPersistentStore() {
         do {
             guard let url = persistentFileURL else { return }
@@ -19,7 +43,7 @@ class MemoryController {
             NSLog("\(error)")
         }
     }
-    
+
     func loadFromPersistentStore() {
         do {
             guard let url = persistentFileURL else { return }
@@ -30,12 +54,12 @@ class MemoryController {
             NSLog("\(error)")
         }
     }
-    
+
     var persistentFileURL: URL? {
         let fm = FileManager.default
-        guard let docDir = fm.urls(for: .documentDirectory, in: .userDomainMask).first else  { return nil }
+        guard let docDir = fm.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
         return docDir.appendingPathComponent("Memories.plist")
     }
-    
+
     var memories: [Memory] = []
 }
