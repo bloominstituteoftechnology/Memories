f2d9993d49eb480f59f2c05ed61b646380a19b91
Conner Alegre <cga0@pm.me>
2018-08-01 16:36:53 -0600

    Setup local notifications

--- Memories/Memories/Helpers/LocalNotificationHelper.swift
+++ Memories/Memories/Helpers/LocalNotificationHelper.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+import UserNotifications
+
+class LocalNotificationHelper {
+    func getAuthorizationStatus(completion: @escaping (UNAuthorizationStatus) -> Void) {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            DispatchQueue.main.async {
+                completion(settings.authorizationStatus)
+            }
+        }
+    }
+
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+
+            if let error = error { NSLog("Error requesting authorization status for local notifications: \(error)") }
+
+            DispatchQueue.main.async {
+                completion(success)
+            }
+        }
+    }
+
+    func scheduleDailyReminderNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Create a memory for today!"
+        content.sound = UNNotificationSound.default
+
+        var date = DateComponents()
+        date.hour = 8
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+
+        let request = UNNotificationRequest(identifier: "MemoryReminder", content: content, trigger: trigger)
+
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                NSLog("There was an error with daily memory reminder notification: \(error)")
+            }
+        }
+    }
+}
+
+
--- Memories/Memories/View Controllers/OnboardingViewController.swift
+++ Memories/Memories/View Controllers/OnboardingViewController.swift
@@ -11,8 +11,20 @@ import UIKit
 class OnboardingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        localNotificationHelper.getAuthorizationStatus { (status) in
+            if status == .authorized {
+                self.performSegue(withIdentifier: "JoinMemories", sender: nil)
+            }
+        }
     }
     
     @IBAction func getStarted(_ sender: Any) {
+        localNotificationHelper.requestAuthorization { (success) in
+            self.localNotificationHelper.scheduleDailyReminderNotification()
+            self.performSegue(withIdentifier: "JoinMemories", sender: nil)
+        }
+        
     }
+    
+    let localNotificationHelper = LocalNotificationHelper()
 }
