6589accc2d98916cfe06df835787392bf713704a
stdemicheli <stdemicheli@gmail.com>
2018-08-01 21:39:19 +0200

    Defined a memory model and implemented a model controller that includes persistence and CRUD methods

---
+++ Memories/Memories/Models/Memory.swift
@@ -0,0 +1,17 @@
+//
+//  Memory.swift
+//  Memories
+//
+//  Created by De MicheliStefano on 01.08.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import Foundation
+
+struct Memory: Codable, Equatable {
+    
+    var title: String
+    var bodyText: String
+    var imageData: Data
+    
+}
