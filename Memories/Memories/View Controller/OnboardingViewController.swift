c7a4988901996e932ec4b1d7e73404e74e473c5b
Iyin Raphael <iyinraphael@gmail.com>
2018-08-08 15:56:16 -0700

    added code to onboardViewController

--- Memories/Memories/View Controller/OnboardingViewController.swift
+++ Memories/Memories/View Controller/OnboardingViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    let localNotificationHelper = LocalNotificationHelper()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        localNotificationHelper.getAuthorizationStatus { (status) in
+            guard status == .authorized else {return}
+            
+            self.performSegue(withIdentifier: "getStarted", sender: nil)
+             
+        }
     }
+        
     @IBAction func getStartedButton(_ sender: Any) {
+        localNotificationHelper.requestAuthorization { (success) in
+            
+        guard success else {return}
+            
+        self.localNotificationHelper.scheduleDailyReminderNotification()
+        self.performSegue(withIdentifier: "getStarted", sender: nil)
+        }
     }
     
  
