258a64db884a7860da1d4bb78724a7f69146b4a6
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-08 18:27:38 -0400

    Started setup for OnboardingViewController

--- Memories/Memories/View Controllers/OnboardingViewController.swift
+++ Memories/Memories/View Controllers/OnboardingViewController.swift
@@ -7,33 +7,30 @@
 //
 
 import UIKit
+import UserNotifications
 
 class OnboardingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        localNotificationHelper.getAuthorizationStatus { (status) in
+            guard status == .authorized else { return }
+            //segue
+            
+        }
     }
     
     @IBAction func startButton(_ sender: Any) {
-        
+        localNotificationHelper.requestAuthorization { (success) in
+            if success {
+            //run the segue
+                //run the function for notification
+            }else {
+                //dont do anything, we hate this user
+            }
+        }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     let localNotificationHelper = LocalNotificationHelper()
 
