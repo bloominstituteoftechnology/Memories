a5020734a62edc6e10b4d1790005eb77b53a681e
Iyin Raphael <iyinraphael@gmail.com>
2018-08-08 14:37:54 -0700

    added views stroyboards and viewController

---
+++ Memories/Memories/View Controller/MemoriesDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MemoriesDetailViewController.swift
+//  Memories
+//
+//  Created by Iyin Raphael on 8/8/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class MemoriesDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func addPhotoButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var titleTextFIeld: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+}
