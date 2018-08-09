1c5ddc468bc486ca8d6a24706464e3f530cf3202
Iyin Raphael <iyinraphael@gmail.com>
2018-08-09 08:19:15 -0700

    prelimenary

--- Memories/Memories/View Controller/MemoriesDetailViewController.swift
+++ Memories/Memories/View Controller/MemoriesDetailViewController.swift
@@ -17,7 +17,33 @@ class MemoriesDetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    
+    func updateViews(){
+        guard isViewLoaded else {return}
+        if let memory = memory{
+            navigationController?.title = "Edit Memory"
+            titleTextFIeld.text = memory.title
+            textView.text = memory.bodyText
+            guard let image: UIImage = UIImage(data: memory.imageData) else {return}
+            imageView.image = image
+        }else{
+            navigationController?.title = "Add Memory"
+        }
+    }
+    
+    
+    @IBAction func SaveMemory(_ sender: Any) {
+        guard let title = titleTextFIeld.text,
+        let bodyText = textView.text,
+            let imageData: Data = imageView.image as? Data else {return}
+        
+        if let memory = memory{
+            memoryController.updateMemory(memory: memory, title: title, bodyText: bodyText, imageData: Data(imageData))
+        }
+        
+        
+        
+    }
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func addPhotoButton(_ sender: Any) {
