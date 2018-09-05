45f1b8c87dca1e51865460f62ab5049e183a2d86
Moin Uddin <U_Moin75@yahoo.com>
2018-09-05 16:08:36 -0400

    Create Memory struct and MemoryController

---
+++ Memories/Memories/Memory.swift
@@ -0,0 +1,17 @@
+//
+//  Memory.swift
+//  Memories
+//
+//  Created by Moin Uddin on 9/5/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import Foundation
+
+struct Memory: Codable, Equatable {
+    
+    var title: String
+    var bodyText: String
+    var imageData: Data
+    
+}
