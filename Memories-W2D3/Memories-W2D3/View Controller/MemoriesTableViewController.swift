a953f58a0181bbd1197facc0ac3df21600861df9
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-12 16:51:23 -0500

    Storyboard set up completed. Realted Cocoa subclass files created. Outlets, actions connected

---
+++ Memories-W2D3/Memories-W2D3/View Controller/MemoriesTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MemoriesTableViewController.swift
+//  Memories-W2D3
+//
+//  Created by Ilgar Ilyasov on 9/12/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MemoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        
+        return cell
+    }
+
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+}
---
+++ Memories-W2D3/Memories-W2D3/View Controller/MemoryDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MemoryDetailViewController.swift
+//  Memories-W2D3
+//
+//  Created by Ilgar Ilyasov on 9/12/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MemoryDetailViewController: UIViewController {
+
+    @IBOutlet weak var memoryImageView: UIImageView!
+    @IBOutlet weak var memoryTextField: UITextField!
+    @IBOutlet weak var memoryTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addPhotoButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveBarButtonTapped(_ sender: Any) {
+    }
+}
