8f6459e92334ade0bce4a46d76c450ec6c86a648
Scott Bennett <stt.btt@gmail.com>
2018-09-12 16:59:20 -0400

    Storyboards setup

---
+++ Memories/Memories/View Controllers/MemoryDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MemoryDetailViewController.swift
+//  Memories
+//
+//  Created by Scott Bennett on 9/12/18.
+//  Copyright © 2018 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class MemoryDetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addPhotoButton(_ sender: Any) {
+    }
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+}
---
+++ Memories/Memories/View Controllers/OnboardingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  OnboardingViewController.swift
+//  Memories
+//
+//  Created by Scott Bennett on 9/12/18.
+//  Copyright © 2018 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func getStartedButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
