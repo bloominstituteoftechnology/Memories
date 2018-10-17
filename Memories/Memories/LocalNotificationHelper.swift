dbd40f5a26f2258d359128d58776a27487194214
Moses Robinson <mosesjrobinson@icloud.com>
2018-10-16 17:34:43 -0700

    created outlets and actions for detail view controller and created localbotif helper

---
+++ Memories/Memories/LocalNotificationHelper.swift
@@ -0,0 +1,9 @@
+//
+//  LocalNotificationHelper.swift
+//  Memories
+//
+//  Created by Moses Robinson on 10/16/18.
+//  Copyright © 2018 Moses Robinson. All rights reserved.
+//
+
+import Foundation
---
+++ Memories/Memories/MemoryDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MemoryDetailViewController.swift
+//  Memories
+//
+//  Created by Moses Robinson on 10/16/18.
+//  Copyright © 2018 Moses Robinson. All rights reserved.
+//
+
+import UIKit
+
+class MemoryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func addPhotoButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+}
