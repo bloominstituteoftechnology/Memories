ec3efb7071aa5a27d4b6a7c129ec494b2704447f
yzhukovs <yzhukovs@bumnetworks.com>
2018-10-17 10:56:08 -0400

    Detail View controller updates

--- Memories/Memories/MemoryDetailViewController.swift
+++ Memories/Memories/MemoryDetailViewController.swift
@@ -9,35 +9,53 @@
 import UIKit
 
 class MemoryDetailViewController: UIViewController {
-
+    
     var memory: Memory?
     var memoryController: MemoryController
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    func updateViews(memory:Memory?){
+        if memory == nil {
+            self.title = "Add Memory"
+        } else {
+            guard let unwrMemory = memory else {return}
+            self.title = "Edit Memory"
+            
+            imageView.image = UIImage(data: unwrMemory.imageData)
+            textField.text = unwrMemory.title
+            textView.text = unwrMemory.bodyText
+            
+        }
+    }
+    
 
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
-    
-    @IBOutlet weak var textField: UITextField!
-    
-    
-    
-    @IBOutlet weak var textView: UITextView!
-    
-    
-    @IBAction func addPhoto(_ sender: Any) {
-    }
-   
-    
-    @IBAction func save(_ sender: Any) {
-    }
-    
+
+
+
+@IBOutlet weak var imageView: UIImageView!
+
+
+@IBOutlet weak var textField: UITextField!
+
+
+
+@IBOutlet weak var textView: UITextView!
+
+
+@IBAction func addPhoto(_ sender: Any) {
+}
+
+
+@IBAction func save(_ sender: Any) {
+}
 
 }
+
