6b3b6b684c2821c354cb207decd9157d9f2b6d54
Moses Robinson <mosesjrobinson@icloud.com>
2018-10-16 18:49:42 -0700

    completed onboarding code

--- Memories/Memories/LocalNotificationHelper.swift
+++ Memories/Memories/LocalNotificationHelper.swift
@@ -1,9 +1,44 @@
-//
-//  LocalNotificationHelper.swift
-//  Memories
-//
-//  Created by Moses Robinson on 10/16/18.
-//  Copyright © 2018 Moses Robinson. All rights reserved.
-//
+import UIKit
+import UserNotifications
 
-import Foundation
+class LocalNotificationHelper {
+    
+    func getAuthorizationStatus(completion: @escaping (UNAuthorizationStatus) -> Void) {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            DispatchQueue.main.async {
+                completion(settings.authorizationStatus)
+            }
+        }
+    }
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            
+            if let error = error { NSLog("Error requesting authorization status for local notifications: \(error)") }
+            
+            DispatchQueue.main.async {
+                completion(success)
+            }
+        }
+    }
+    
+    func scheduleDailyReminderNotification() {
+        let identifier = "memoryReminderNotification"
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Memory Reminder"
+        content.body = "Hey! Add a new Memory for today."
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 20
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                NSLog("Notification Trigger Error: \(error)")
+            }
+        }
+    }
+}
--- Memories/Memories/OnboardingViewController.swift
+++ Memories/Memories/OnboardingViewController.swift
@@ -1,13 +1,29 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    
+    let localNotificationHelper = LocalNotificationHelper()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        localNotificationHelper.getAuthorizationStatus { isAuthorized in
+            if isAuthorized == isAuthorized {
+                self.performSegue(withIdentifier: "onboardingSegue", sender: nil)
+            }
+        }
     }
     
+    // when this button is pressed, it will request for permission
     @IBAction func getStartedButton(_ sender: Any) {
         
+        //if this is succesfully done, segue
+        localNotificationHelper.requestAuthorization { successful in
+            if successful {
+                
+                // this is calling the function from location helper, that starts reminder
+                self.localNotificationHelper.scheduleDailyReminderNotification()
+                self.performSegue(withIdentifier: "onboardingSegue", sender: nil)
+            }
+        }
     }
-    
 }
