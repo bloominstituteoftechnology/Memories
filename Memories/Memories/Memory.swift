52a1297ea6b435b427dcb2d76105d96333147d5c
Sean Doyle <sdoylelambda@gmail.com>
2018-10-24 16:30:47 -0400

    initial commit

--- Memories/Memories/Memory.swift
+++ Memories/Memories/Memory.swift
@@ -1,6 +1,19 @@
 import Foundation
 
-struct Memory: Codable, Equatable {
+//struct Memory: Codable, Equatable {
+//    var title: String
+//    var bodyText: String
+//    var imageData: Data
+//
+//    init(title: String, bodyText: String, imageData: Data) {
+//        self.title = title
+//        self.bodyText = bodyText
+//        self.imageData = imageData
+//    }
+//}
+//
+
+struct Memory: Equatable, Codable {
     var title: String
     var bodyText: String
     var imageData: Data
