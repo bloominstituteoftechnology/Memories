c6f022b44938ceab8e0559132b76d50fc41585d1
Frulwinn <frulwinn@gmail.com>
2018-10-24 20:01:16 -0700

    Added elements on storyboard for MemoryDetailViewController and then connected outlets to swift file

---
+++ Memories/LocalNotificationHelper.swift
@@ -0,0 +1,9 @@
+//
+//  LocalNotificationHelper.swift
+//  Memories
+//
+//  Created by Lambda_School_Loaner_34 on 10/24/18.
+//  Copyright © 2018 Frulwinn Collick. All rights reserved.
+//
+
+import Foundation
---
+++ Memories/MemoryDetailViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class MemoryDetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var photoTitle: UITextField!
+    
+    @IBOutlet weak var photoText: UITextView!
+    
+    @IBAction func addButton(_ sender: Any) {
+    }
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
