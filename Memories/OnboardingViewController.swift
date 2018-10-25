7475c3266961c389bc082c30f20757b56177574c
Frulwinn <frulwinn@gmail.com>
2018-10-24 19:26:12 -0700

    Added OnboardingViewController

---
+++ Memories/OnboardingViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    @IBAction func getStarted(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
