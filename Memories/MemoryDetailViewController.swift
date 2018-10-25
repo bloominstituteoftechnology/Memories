085cb70d1630cb67421b4163d1ded35c87e78537
Frulwinn <frulwinn@gmail.com>
2018-10-24 22:10:29 -0700

    Added MemoriesTableViewController

--- Memories/MemoryDetailViewController.swift
+++ Memories/MemoryDetailViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
+import Photos
 
-class MemoryDetailViewController: UIViewController {
-
+class MemoryDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var memory: Memory?
+    var memoryController : MemoryController?
+    
     @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var photoTitle: UITextField!
@@ -9,27 +13,67 @@ class MemoryDetailViewController: UIViewController {
     @IBOutlet weak var photoText: UITextView!
     
     @IBAction func addButton(_ sender: Any) {
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        switch authorizationStatus {
+        case .authorized:
+            presentImagePickerController()
+        default:
+            PHPhotoLibrary.requestAuthorization { status in
+                if status == .authorized {
+                    self.presentImagePickerController()
+                }
+            }
+        }
     }
     @IBAction func saveButton(_ sender: Any) {
+        guard let memoryController = memoryController else { return }
+        guard let titleText = photoTitle.text,
+            let bodyText = photoText.text,
+            let image = imageView.image,
+            let imgData = image.pngData() else { return }
+        
+        guard let memory = memory else {
+            memoryController.createMemory(with: titleText, bodyText: bodyText, imageData: imgData)
+            navigationController?.popViewController(animated: true)
+            return
+        }
+        memoryController.update(memory: memory, title: titleText, bodyText: bodyText, imageData: imgData)
+        navigationController?.popViewController(animated: true)
+        
     }
     
-    
+    //unwrapping the memory object
+    func updateViews() {
+        guard let memory = memory else { return }
+        title = memory.title
+        photoTitle.text = memory.title
+        photoText.text = memory.bodyText
+        guard let memoryImage = UIImage(data: memory.imageData) else { return }
+        imageView.image = memoryImage
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateViews()
     }
-    */
-
+    
+    private func presentImagePickerController() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        } else { return }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[.originalImage] as? UIImage else {return}
+        imageView.image = image
+    }
 }
